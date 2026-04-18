22bf65a1e202243fdea9133e9ff6ed4d43247a4b
Rondi650 <rondinellenunes123@gmail.com>
2026-04-18 16:29:26 -0300

    Refactor Docker Compose and startup script for improved MySQL handling

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -4,12 +4,13 @@
 set -e
 
 while ! nc -z $MYSQL_HOST $MYSQL_PORT; do
-  echo "🟡 Waiting for MySQL Database Startup ($MYSQL_HOST $MYSQL_PORT) ..." &
-  sleep 1
+  echo "🟡 Waiting for MySQL Database Startup ($MYSQL_HOST $MYSQL_PORT) ..."
+  sleep 2
 done
 
 echo "✅ MySQL Database Started Successfully ($MYSQL_HOST:$MYSQL_PORT)"
 
 python3 manage.py collectstatic --noinput
-python3 manage.py migrate
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 python3 manage.py runserver 0.0.0.0:8000
