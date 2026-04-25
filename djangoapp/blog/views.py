2439a05f2cfc1492d79c2f425fd8460c5c4f9bb0
Rondi650 <rondinellenunes123@gmail.com>
2026-04-25 00:23:46 -0300

    Implement PostManager for published posts, update index view for pagination, and modify templates for post display

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,11 +1,14 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.http import HttpRequest
+from blog.models import Post
 
-posts = list(range(10))
+PER_PAGE = 9
 
 
 def index(request: HttpRequest):
+    posts = Post.objects.get_published()
+
     paginator = Paginator(posts, 9)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
@@ -20,28 +23,20 @@ def index(request: HttpRequest):
 
 
 def page(request: HttpRequest):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
     return render(
         request,
         'blog/pages/page.html',
         {
-            'page_obj': page_obj,
+            # 'page_obj': page_obj,
         }
     )
 
 
 def post(request: HttpRequest):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
     return render(
         request,
         'blog/pages/post.html',
         {
-            'page_obj': page_obj,
+            # 'page_obj': page_obj,
         }
     )
