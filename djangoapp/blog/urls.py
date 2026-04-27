362971c2812dc905624da32bacdd789bd1a92102
Rondi650 <rondinellenunes123@gmail.com>
2026-04-27 20:41:09 -0300

    refactor(blog): migrate tag view to TagListView

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,5 +1,6 @@
 from blog.views import (PostListView, page,
-                        post, search, tag, CreatedByListView, CategoryListView)
+                        post, search, TagListView, CreatedByListView,
+                        CategoryListView)
 from django.urls import path
 
 app_name = 'blog'
@@ -11,6 +12,6 @@ urlpatterns = [
     path('created_by/<int:author_pk>/',
          CreatedByListView.as_view(), name='created_by'),
     path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
-    path('tag/<slug:slug>/', tag, name='tag'),
+    path('tag/<slug:slug>/', TagListView.as_view(), name='tag'),
     path('search/', search, name='search'),
 ]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -89,26 +89,24 @@ class CategoryListView(PostListView):
         return ctx
 
 
-def tag(request: HttpRequest, slug: str) -> HttpResponse:
-    posts: QuerySet[Post] = (
-        cast(PostManager, Post.objects).get_published()
-        .filter(tags__slug=slug)
-    )
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+class TagListView(PostListView):
+    allow_empty = False
 
-    if len(page_obj) == 0:
-        raise Http404()
-    page_title = f'{page_obj[0].tags.first().name} - Tag - '
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(
+            tags__slug=self.kwargs.get('slug')
+        )
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        page_title = (
+            f'{self.object_list[0].tags.first().name}'  # type: ignore
+            ' - Tag - '
+        )
+        ctx.update({
             'page_title': page_title,
-        }
-    )
+        })
+        return ctx
 
 
 def search(request: HttpRequest) -> HttpResponse:
