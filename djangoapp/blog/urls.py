be76f0387248b0c0facfece2d139f26e4c55e0a8
Rondi650 <rondinellenunes123@gmail.com>
2026-04-27 20:31:31 -0300

    refactor(blog): migrate category view to CategoryListView

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,4 +1,5 @@
-from blog.views import category, PostListView, page, post, search, tag, CreatedByListView
+from blog.views import (PostListView, page,
+                        post, search, tag, CreatedByListView, CategoryListView)
 from django.urls import path
 
 app_name = 'blog'
@@ -9,7 +10,7 @@ urlpatterns = [
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/',
          CreatedByListView.as_view(), name='created_by'),
-    path('category/<slug:slug>/', category, name='category'),
+    path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
     path('tag/<slug:slug>/', tag, name='tag'),
     path('search/', search, name='search'),
 ]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -69,27 +69,24 @@ class CreatedByListView(PostListView):
         return super().get(request, *args, **kwargs)
 
 
-def category(request: HttpRequest, slug: str) -> HttpResponse:
-    posts: QuerySet[Post] = (
-        cast(PostManager, Post.objects).get_published()
-        .filter(category__slug=slug)
-    )
+class CategoryListView(PostListView):
+    allow_empty = False
 
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(
+            category__slug=self.kwargs.get('slug')
+        )
 
-    if len(page_obj) == 0:
-        raise Http404()
-    page_title = f'{page_obj[0].category.name} - Categoria - '
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        page_title = (
+            f'{self.object_list[0].category.name}'  # type: ignore
+            ' - Categoria - '
+        )
+        ctx.update({
             'page_title': page_title,
-        }
-    )
+        })
+        return ctx
 
 
 def tag(request: HttpRequest, slug: str) -> HttpResponse:
