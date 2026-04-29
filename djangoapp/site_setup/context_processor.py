3705eb689e639afdad68b8cf261bc34b29995104
Rondi650 <rondinellenunes123@gmail.com>
2026-04-29 11:20:58 -0300

    removido print do site setup para testes

--- djangoapp/site_setup/context_processor.py
+++ djangoapp/site_setup/context_processor.py
@@ -5,23 +5,6 @@ from site_setup.models import SiteSetup
 def site_setup(request: HttpRequest):
     setup = SiteSetup.objects.order_by('-id').first()
 
-    # Inicio debug
-    print("-"*50, "\n", "SITE_SETUP MENU LINKS", sep="")
-    if setup:
-        for link in setup.menu_links.all():  # type:ignore
-            print(link)
-
-    print("-"*50, "\n", "SITE_SETUP FIELDS", sep="")
-    for chave, valor in setup.__dict__.items():
-        print(chave, ": ", valor)
-    print("-"*50)
-    # Fim do debug
-    
-    if setup is None:
-        return {}  # retorna contexto vazio, sem quebrar
-
-    context = {}
-    for chave, valor in setup.__dict__.items():
-        context[chave] = valor
-
-    return context
+    return {
+        'site_setup': setup,
+    }
