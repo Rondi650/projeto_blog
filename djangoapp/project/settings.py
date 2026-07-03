fc5ef000a9e4a1479c76d33172f54a1e3e389170
Rondi650 <rondinellenunes123@gmail.com>
2026-07-03 19:40:27 -0300

    feat(docker): update Docker setup for PostgreSQL and remove MySQL configuration

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -105,26 +105,6 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.getenv('MYSQL_DB'),
-#         'USER': os.getenv('MYSQL_USER'),
-#         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-#         'HOST': os.getenv('MYSQL_HOST'),
-#         'PORT': os.getenv('MYSQL_PORT'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
